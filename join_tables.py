9ff39b2f6d5d764a2ca0f2b14528470b4a459e5b
“serbier” <sergioacr94@gmail.com>
2025-09-30 15:05:46 -0500

    FIX naming bug

--- join_tables.py
+++ join_tables.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob as glob
 import os
 
-qtl_map = pd.read_csv('./config/tg_varians_FFARV3.txt', sep='\t')
+qtl_map = pd.read_csv('./config/target_variants.txt', sep='\t')
 qtl_map.rename(columns={'variant_id': 'qtn_name'}, inplace=True)
 primer_paths = glob.glob('./results/*/summary/*_primers.csv')
 combs_paths = glob.glob('./results/*/summary/*_combinations.csv')
